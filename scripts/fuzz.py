4b2cdbff7fb8a8acf8995351027d511ee511eec1
alligator <reece@alligatr.co.uk>
2026-08-01 16:32:23 +0100

    add -f argument to fuzz.py

--- scripts/fuzz.py
+++ scripts/fuzz.py
@@ -4,10 +4,11 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--parallel', type=int, required=True)
 parser.add_argument('-t', '--fuzztime', default='30m')
+parser.add_argument('-f', default='FuzzJqawkWithJson')
 
 args = parser.parse_args()
 
-goargs = ['go', 'test', f'-parallel={args.parallel}', '-fuzz=FuzzJqawkWithJson']
+goargs = ['go', 'test', f'-parallel={args.parallel}', f'-fuzz={args.f}']
 if args.fuzztime is not None:
     goargs.append(f'-fuzztime={args.fuzztime}')
 
